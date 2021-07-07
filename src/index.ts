f0cfb2fe1bc63f41004186b630eee8ee69d67026
Cambo45 <65762501+Cambo45@users.noreply.github.com>
2021-07-07 17:03:22 +1200

    declared types

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import express from "express";
-const app = express();
-const PORT = 3000;
+const app: express.Express = express();
+const PORT: number = 3000;
 
-app.get('/', (req,res) => res.send('Timecloud Test Server'));
+app.get('/', (req: express.Request, res: express.Response) => res.send('Timecloud Test Server'));
 
-app.get('/data', (req,res) => {
-    const message : object = {success: true, message: "data you requested"}
+app.get('/data', (req: express.Request, res: express.Response) => {
+    const message: object = {success: true, message: "data you requested"}
     res.json(message)
 });
 
-app.get('/abcd', (req,res) => {
-    const message : object = {success: true, message: "efgh"}
+app.get('/data/abcd', (req: express.Request, res: express.Response) => {
+    const message: object = {success: true, message: "efgh"}
     res.json(message)
 });
 
-app.use(function (req, res) {
+app.use((req: express.Request, res: express.Response) => {
     res.status(404).json({success: false, message: "bad request"})
-  })
+});
 
 app.listen(PORT, () => {
     console.log(`server is running at https://localhost:${PORT}`)
